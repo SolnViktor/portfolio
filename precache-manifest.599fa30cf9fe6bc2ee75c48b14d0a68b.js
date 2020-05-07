5d55873b1bc62ef5559f8f7179bc006a2f042fb4
SolnViktor <solnviktor@gmail.com>
2020-05-07 18:29:34 +0300

    Updates

---
+++ precache-manifest.599fa30cf9fe6bc2ee75c48b14d0a68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8434b3ef2c80c11efb69bce8c95d6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "300bbd6ce09fbf84ba17",
+    "url": "/portfolio/static/css/main.281022f1.chunk.css"
+  },
+  {
+    "revision": "939f79b78365eb70a8d4",
+    "url": "/portfolio/static/js/2.937e1265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.937e1265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300bbd6ce09fbf84ba17",
+    "url": "/portfolio/static/js/main.63b806bd.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "ffc71ce5712b2b5ff49d91d0c1420b04",
+    "url": "/portfolio/static/media/photo.ffc71ce5.jpg"
+  }
+]);
