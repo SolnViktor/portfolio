1c77d14bc430dac4cd9780e97b0168abe7c66c0e
SolnViktor <solnviktor@gmail.com>
2020-05-29 17:04:41 +0300

    Updates

---
+++ precache-manifest.78df9cbe0369913eaf3c78ff494e17e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c88d1d02ecf7f03f7cfcd54a26168f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1581055b402cfb17ef5",
+    "url": "/portfolio/static/css/main.1ec9937c.chunk.css"
+  },
+  {
+    "revision": "093f015a046ed6aaf73a",
+    "url": "/portfolio/static/js/2.f9725426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f9725426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1581055b402cfb17ef5",
+    "url": "/portfolio/static/js/main.deeb224b.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "345a8f64308a94731169573ca670a681",
+    "url": "/portfolio/static/media/bg4.345a8f64.jpg"
+  },
+  {
+    "revision": "a10b802dacbd1b29221e2aea95407aad",
+    "url": "/portfolio/static/media/debut-dark.a10b802d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c13a523810aa7c88d868d3d6809421a9.js"
+  "/portfolio/precache-manifest.78df9cbe0369913eaf3c78ff494e17e2.js"
 );
 
 self.addEventListener('message', (event) => {
